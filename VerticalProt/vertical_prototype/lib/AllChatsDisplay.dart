bc734b736f938cb94aa5d9f7b10a0f17b8e3be2e
afs202005277 <up202005277@up.pt>
2022-04-09 22:13:43 +0100

    Trying to move things to the right

--- VerticalProt/vertical_prototype/lib/AllChatsDisplay.dart
+++ VerticalProt/vertical_prototype/lib/AllChatsDisplay.dart
@@ -4,6 +4,15 @@ import 'SingleChatDisplay.dart';
 class ChatDisplay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChatDisplay();
+    return Scaffold(
+        appBar: AppBar(title: const Text('UniChat')),
+        body: MediaQuery.removePadding(
+          context: context,
+          removeLeft: true,
+          removeBottom: true,
+          removeRight: true,
+          removeTop: true,
+          child: SingleChatDisplay(),
+        ));
   }
 }
--- VerticalProt/vertical_prototype/lib/SingleChatDisplay.dart
+++ VerticalProt/vertical_prototype/lib/SingleChatDisplay.dart
@@ -3,16 +3,26 @@ import 'package:flutter/material.dart';
 class SingleChatDisplay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('UniChat')),
-      body: Row(
-        children: const [
-          Text('DA'),
-          Text('Desenho de Algoritmos'),
-          Text('2LEIC03'),
-          Text('11'),
-        ],
-      ),
-    );
+    double currrentWidth = MediaQuery.of(context).size.width;
+    return Container(
+        height: 100.0,
+        width: currrentWidth,
+        padding: EdgeInsets.zero,
+        color: Colors.red,
+        child: Row(
+          children: const [
+            Align(alignment: Alignment.centerLeft, child: Text('DA')),
+            Align(
+                alignment: Alignment.center,
+                child: Text('Desenho de Algoritmos')),
+            Align(alignment: Alignment.center, child: Text('2LEIC03')),
+            Align(alignment: Alignment.bottomRight, child: Text('11')),
+            Align(
+              alignment: Alignment(1, 1),
+              child: Text("teste"),
+            )
+          ],
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+        ));
   }
 }
