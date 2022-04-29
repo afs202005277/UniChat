92f82d3f52dd982274aca265feb5585e18ae703f
afs202005277 <up202005277@up.pt>
2022-04-29 15:31:12 +0100

    Changes to student class

--- VerticalProt/vertical_prototype/lib/student_info.dart
+++ VerticalProt/vertical_prototype/lib/student_info.dart
@@ -2,13 +2,19 @@ import 'sigarra_api_handler.dart';
 import 'class_info.dart';
 
 class Student {
-  int id = 0;
-  String nome = "";
-  List<Class> classes = [];
-  Student(int id1, String nome1,List<Class> classes1){
-    this.id = id1;
-    this.nome = nome1;
-    this.classes = SigarraAPIhandler.getStudentClasses(this.id);
+  static int id = 0;
+  static String nome = "";
+  static List<Class> classes = [];
+  static Student? instance;
+
+  static _Student(int id1, String nome1) {
+    id = id1;
+    nome = nome1;
+    classes = SigarraAPIhandler.getStudentClasses(id);
+  }
+
+  static Student? singleton(int id, String nome) {
+    instance ??= _Student(id, nome);
+    return instance;
   }
 }
-
