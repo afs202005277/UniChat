d3a568a6fd38bb00f0ac1c75757809fd151ccfac
afs202005277 <up202005277@up.pt>
2022-05-12 18:49:09 +0100

    Removed unused imports

--- VerticalProt/vertical_prototype/test/widget_test.dart
+++ VerticalProt/vertical_prototype/test/widget_test.dart
@@ -3,19 +3,17 @@ import 'package:vertical_prototype/chat_info.dart';
 import 'package:vertical_prototype/get_chat_list.dart';
 
 import 'package:vertical_prototype/main.dart';
-import 'package:vertical_prototype/top_bar.dart';
-import 'package:vertical_prototype/all_chats_display.dart';
 
 void main() {
   testWidgets('Home Page Test', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
     expect(find.text("UNICHAT"), findsOneWidget);
-    for(ChatInfo chatinfo in GetChatList.fetchAll(1)){
-        expect(find.text(chatinfo.nomeCompleto), findsOneWidget);
-        expect(find.text(chatinfo.numParticipantes.toString()), findsOneWidget);
-        expect(find.text(chatinfo.turma), findsOneWidget);
-        expect(find.text(chatinfo.sigla), findsOneWidget);
+    for (ChatInfo chatinfo in GetChatList.fetchAll(1)) {
+      expect(find.text(chatinfo.nomeCompleto), findsOneWidget);
+      expect(find.text(chatinfo.numParticipantes.toString()), findsOneWidget);
+      expect(find.text(chatinfo.turma), findsOneWidget);
+      expect(find.text(chatinfo.sigla), findsOneWidget);
     }
   });
 }
