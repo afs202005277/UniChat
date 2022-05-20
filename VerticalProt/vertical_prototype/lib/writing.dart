3f73625fe9e129497298cff6ce4db7d7153d99b1
up202007044 <up202007044@up.pt>
2022-05-20 14:22:16 +0100

    Message appearence update

--- VerticalProt/vertical_prototype/lib/writing.dart
+++ VerticalProt/vertical_prototype/lib/writing.dart
@@ -23,14 +23,57 @@ class Writing extends StatelessWidget {
               if (!snapshot.hasData) return const SizedBox.shrink();
               return ListView.builder(
                 itemCount: snapshot.data!.docs.length,
+                padding: EdgeInsets.only(bottom: 70,top:10),
+                shrinkWrap: true,
                 itemBuilder: (BuildContext context, int index) {
                   final docData = snapshot.data?.docs[index].data() as Map<
                       String,
                       dynamic>; // o metodo builder está a iterar pelas mensagens. A variavel docData tem a mensagem que estas a ler neste momento, no formato userName => conteudo da mensagem (é como se fosse um dicionario do python)
-                  return ListTile(
-                    title: Text(docData[
-                        'Andre']), // acede ao conteudo da mensagem do user Andre e manda isso para o ecra. A decoracao da mensagem deve ser feita aqui
-                  );
+                  return Container(
+                          padding: EdgeInsets.symmetric(vertical: 10,horizontal: 14),
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Container(
+                              constraints: BoxConstraints(
+                                maxWidth: MediaQuery.of(context).size.width/2,
+                              ),
+                              decoration:  BoxDecoration(
+                                borderRadius: BorderRadius.only(topRight: Radius.circular(26),bottomLeft: Radius.circular(26),bottomRight: Radius.circular(26)),
+                                color: Color.fromRGBO(149, 0, 20, 1),
+                              ),
+                              padding: EdgeInsets.all(16),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end  ,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text('Andre',
+                                        style:  const TextStyle(
+                                          fontSize: 12,
+                                          color: Color.fromRGBO(203, 203, 203, 1),
+                                        ),
+                                      ),
+                                      Text(docData['Andre'],
+                                        style: const TextStyle(
+                                          fontSize: 15,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      SizedBox(height: 5,),
+                                    ],
+                                  ),
+                                  Text('20:20',
+                                    style:  const TextStyle(
+                                      fontSize: 12,
+                                      color: Color.fromRGBO(203, 203, 203, 1),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                  );// acede ao conteudo da mensagem do user Andre e manda isso para o ecra. A decoracao da mensagem deve ser feita aqui
                 },
               );
             },
